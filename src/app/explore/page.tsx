84c92ccc332113f1c032ca32f76c48c9ac0201a9
Raj Varsani <varasaniraj5@gmail.com>
2024-08-15 03:35:48 +0530

    restructure app

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -2,5 +2,10 @@ import TraceDetailsSheet from "@/components/explore/TraceDetailsSheet";
 import TracesTreeView from "@/components/explore/TracesTreeView";
 
 export default function Page() {
-  return <main className="w-screen h-screen">Dashboard</main>;
+  return (
+    <main className="w-screen h-screen">
+      <TracesTreeView />
+      <TraceDetailsSheet />
+    </main>
+  );
 }
